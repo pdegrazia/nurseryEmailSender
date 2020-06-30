e106e6287a22158f94f75c611921e9de0b670e17
Paolo <p.degrazia@gmail.com>
2020-06-30 21:57:41 +0100

    removed sender to prevent going to spam (known bug); using basedir for file paths

--- src/main.py
+++ src/main.py
@@ -13,11 +13,12 @@ import config
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/gmail.send']
 
+#BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR='/home/pi/workspace/nurseryEmailSender/src/'
 
 def create_message(sender, to, subject, message_text):
     message = MIMEText(message_text)
     message['to'] = to
-    message['from'] = sender
     message['subject'] = subject
     raw_message = base64.urlsafe_b64encode(message.as_string().encode("utf-8"))
     return {
@@ -30,8 +31,9 @@ def main():
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
+    token_file = BASE_DIR + 'token.pickle'
+    if os.path.exists(token_file):
+        with open(token_file, 'rb') as token:
             creds = pickle.load(token)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
@@ -39,18 +41,18 @@ def main():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
+                BASE_DIR + 'credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
+        with open(token_file, 'wb') as token:
             pickle.dump(creds, token)
 
     service = build('gmail', 'v1', credentials=creds)
-    msg = create_message(config.sender, config.recipient, 'test', 'hello')
+    msg = create_message(config.sender, config.recipient, 'Written Feedback - Nora De Grazia', config.email_text)
     try:
         message = service.users().messages().send(userId=config.sender, body=msg).execute()
     except Exception:
-        raise Exception('Something went wrong while sendind email...')
+        raise Exception('Something went wrong while sending email...')
 
 
 if __name__ == '__main__':
